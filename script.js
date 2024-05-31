86d560f7defd6deb379417fc4d3020e7b939c49c
jaydip430 <40359214+jaydip430@users.noreply.github.com>
2024-05-31 15:04:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function appendValue(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function calculateResult() {
+    let result = eval(document.getElementById('result').value);
+    document.getElementById('result').value = result;
+}
